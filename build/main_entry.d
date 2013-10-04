a1d4fa8886bc58cd5aabb5ad3b0fa8befcf0dd65
ryan-bunker <ryan.bunker@gmail.com>
2013-10-04 02:17:10 -0500

    Added build output to .gitignore and removed those files from repository.

--- build/main_entry.d
+++
@@ -1,5 +0,0 @@
-main_entry.o main_entry.d:  ../src/kernel/main_entry.cc \
- /home/ryan/opt/cross/lib/gcc/i586-elf/4.8.1/include/stdint.h \
- /home/ryan/opt/cross/lib/gcc/i586-elf/4.8.1/include/stdint-gcc.h \
- ../src/arch/i586/include/core/addressing.h \
- ../src/include/boot/multiboot.h
